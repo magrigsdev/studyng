c220f9552820a7e1c832ac4ae03098cde78417a0
fridev <magrigsdesign@gmail.com>
2023-09-08 03:21:41 +0200

    database created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+        Schema::create('Users', function (Blueprint $table) {
+            $table->id('id_users');
+            $table->string('nom');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('motpasse');
+            $table->string('status');
+            $table->string('is_manager');
+            $table->date('date_creation');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('Users');
     }
 }
--- database/migrations/2023_09_08_000447_init_table.php
+++ database/migrations/2023_09_08_000447_init_table.php
@@ -14,6 +14,159 @@ class InitTable extends Migration
     public function up()
     {
         //
+            Schema::create('Managers', function (Blueprint $table) {
+            $table->id('id_man');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('email')->unique();
+            $table->string('sexe');
+            $table->text('addresse');
+            
+        });
+
+        //
+            Schema::create('Etudiants', function (Blueprint $table) {
+            $table->id('id_etud');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('email')->unique();
+            $table->string('sexe');
+            $table->date('date_naissance');
+            $table->string('photo');
+            $table->date('date_inscription');
+            
+            
+        });
+
+         //
+            Schema::create('Documents', function (Blueprint $table) {
+            $table->id('id_doc');
+            $table->string('lycee');
+            $table->string('universite');
+            $table->string('master');
+            $table->string('baccalaureat');
+            $table->string('bts');
+            $table->string('licence');
+            $table->string('autre');
+            
+   
+        });
+
+        //
+            Schema::create('Services', function (Blueprint $table) {
+            $table->id('id_serv');
+            $table->string('intituler');
+            $table->date('date_debut');
+            $table->date('date_cloture');   
+   
+        });
+
+            Schema::create('Etablissements', function (Blueprint $table) {
+            $table->id('id_etab');
+            $table->string('intituler');
+             
+   
+        });
+
+            Schema::create('Frais', function (Blueprint $table) {
+            $table->id('id_frais');
+            $table->string('intituler');
+            $table->date('date');
+             
+        });
+
+        //les relations ...
+        Schema::create('SeConnecter', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_users');
+            $table->unsignedBigInteger('id_man');
+            $table->string('status');
+
+            $table->foreign('id_users')->references('id_users')->on('Users');
+            $table->foreign('id_man')->references('id_man')->on('Managers');
+             
+        });
+
+        Schema::create('Consulter', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_doc');
+            $table->unsignedBigInteger('id_man');
+            $table->date('date');
+            $table->string('intituler');
+
+            $table->foreign('id_doc')->references('id_doc')->on('Documents');
+            $table->foreign('id_man')->references('id_man')->on('Managers');
+             
+        });
+
+            Schema::create('Voir', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_etud');
+            $table->unsignedBigInteger('id_man');
+            $table->date('date');
+            $table->string('intituler');
+
+            $table->foreign('id_etud')->references('id_etud')->on('Etudiants');
+            $table->foreign('id_man')->references('id_man')->on('Managers');
+             
+        });
+
+            Schema::create('Deposer', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_doc');
+            $table->unsignedBigInteger('id_etud');
+            $table->date('date_depot');
+            $table->string('status');
+            $table->string('intituler');
+
+            $table->foreign('id_etud')->references('id_etud')->on('Etudiants');
+            $table->foreign('id_doc')->references('id_doc')->on('Documents');
+             
+        });
+
+            Schema::create('Verifier', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_serv');
+            $table->unsignedBigInteger('id_man');
+            $table->date('date_verification');
+            $table->string('intituler');
+
+            $table->foreign('id_serv')->references('id_serv')->on('Services');
+            $table->foreign('id_man')->references('id_man')->on('Managers');
+             
+        });
+
+            Schema::create('Beneficier', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_serv');
+            $table->unsignedBigInteger('id_etud');
+
+            $table->foreign('id_serv')->references('id_serv')->on('Services');
+            $table->foreign('id_etud')->references('id_etud')->on('Etudiants');
+             
+        });
+
+        Schema::create('Choisir', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_etab');
+            $table->unsignedBigInteger('id_etud');
+
+            $table->foreign('id_etab')->references('id_etab')->on('Etablissements');
+            $table->foreign('id_etud')->references('id_etud')->on('Etudiants');
+             
+        });
+
+            Schema::create('Editer', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_serv');
+            $table->unsignedBigInteger('id_man');
+
+            $table->foreign('id_serv')->references('id_serv')->on('Services');
+            $table->foreign('id_man')->references('id_man')->on('Managers');
+             
+        });
+
+            Schema::create('Payer', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_frais');
+            $table->unsignedBigInteger('id_etud');
+
+            $table->foreign('id_etud')->references('id_etud')->on('Etudiants');
+            $table->foreign('id_frais')->references('id_frais')->on('Frais');
+             
+        });
+
     }
 
     /**
@@ -24,5 +177,21 @@ class InitTable extends Migration
     public function down()
     {
         //
+         Schema::dropIfExists('Managers');
+         Schema::dropIfExists('Etudiants');
+         Schema::dropIfExists('Documents');
+         Schema::dropIfExists('Services');
+         Schema::dropIfExists('Etablissements');
+
+         Schema::dropIfExists('Frais');
+         Schema::dropIfExists('Voir');
+         Schema::dropIfExists('Deposer');
+
+         Schema::dropIfExists('Payer');
+         Schema::dropIfExists('Editer');
+         Schema::dropIfExists('Choisir');
+
+         Schema::dropIfExists('Beneficier');
+         Schema::dropIfExists('Verifier');
     }
 }
